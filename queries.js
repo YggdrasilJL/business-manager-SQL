670457414cb24d47531be7137dac41628c798d3d
YggdrasilJL <yggdrasiljl@gmail.com>
2023-07-22 19:43:18 -0300

    roles to employee prompt and update role

--- queries.js
+++ queries.js
@@ -3,10 +3,10 @@ const connection = require('./db/db.js')
 const inquirer = require('inquirer')
 
 async function viewEmployeesQ() {
-    const [rows, fields] = 
-    await connection.promise().query(
-    // uppercase mysql commands to make it look more readable in vscode
-     `SELECT 
+    const [rows, fields] =
+        await connection.promise().query(
+        // uppercase mysql commands to make it look more readable in vscode
+            `SELECT 
         emp.id AS employee_id,
         emp.first_name,
         emp.last_name,
@@ -18,7 +18,7 @@ async function viewEmployeesQ() {
      LEFT JOIN role rl ON emp.role_id = rl.id
      LEFT JOIN department dep ON rl.department_id = dep.id
      LEFT JOIN employee mgr ON emp.manager_id = mgr.id;`
-  )
+        )
     console.table(rows)
 }
 
@@ -38,22 +38,37 @@ async function viewDepartmentsQ() {
 }
 
 async function addEmployeeQ() {
+    const [roles, fields] = await connection.promise().query(
+        'select * from role'
+    )
+    const roleChoices = roles.map(role => ({
+        name: role.title,
+        value: role.id
+    }))
     await inquirer
         .prompt([
             {
                 type: 'input',
-                messsage: "What is the employee's first name?",
+                messsage: "Employee's first name?",
                 name: 'firstName'
             },
             {
                 type: 'input',
-                message: 'Last name?',
+                message: "Employee's last name?",
                 name: 'lastName'
+            },
+            {
+                type: 'list',
+                message: "What is the employee's role?",
+                choices: roleChoices,
+                name: 'role'
             }
         ])
         .then(async data => {
+
             const [rows, fields] = await connection.promise().query(
-                `insert into employee (first_name, last_name) values ('${data.firstName}', '${data.lastName}')`
+                `insert into employee (first_name, last_name, role_id) 
+                  values ('${data.firstName}', '${data.lastName}', '${data.role}')`
             )
             console.info(data.firstName, 'added!')
         })
@@ -80,7 +95,7 @@ async function addRoleQ() {
             await connection.promise().query(
                 `insert into role (title, salary) values ('${data.roleName}','${data.salary}')`
             )
-        console.info(data.roleName, 'role added')
+            console.info(data.roleName, 'role added')
         })
         .catch(err => {
             console.error("error:", err)
@@ -93,6 +108,7 @@ async function updateRoleQ() {
     )
     const employeeChoices = employees.map(employee => ({
         name: `${employee.first_name} ${employee.last_name}`,
+        value: employee.id
     }))
     await inquirer
         .prompt([
@@ -100,11 +116,32 @@ async function updateRoleQ() {
                 type: 'list',
                 message: 'Select the employee you want to update the role of:',
                 choices: employeeChoices,
-                name: 'chooseEmployee'
+                name: 'employeeId'
             }
         ])
         .then(async data => {
-            console.log(data.chooseEmployee)
+            const [roles, fields] = await connection.promise()
+                .query('select * from role')
+            const roleChoices = roles.map(role => ({
+                name: role.title,
+                value: role.id
+            }))
+
+        await inquirer
+            .prompt(
+                {
+                    type: 'list',
+                    message: 'What is the role you want updated for selected Employee?',
+                    choices: roleChoices,
+                    name: 'roleId'
+                }
+            )
+            .then(async roleData => {
+                await connection.promise().query(
+                    'update employee set role_id = ? where id = ?',
+                    [roleData.roleId, data.employeeId]
+                )
+            })
         })
         .catch(err => {
             console.error("error:", err)
@@ -131,5 +168,21 @@ async function addDepartmentQ() {
         })
 }
 
-module.exports = { viewEmployeesQ, viewRolesQ, viewDepartmentsQ, addEmployeeQ,
-     updateRoleQ, addDepartmentQ, addRoleQ,  }
+async function seeBudgetQ() {
+        await connection.promise().query(
+            'delete from budget'
+        )
+        await connection.promise().query(
+        `insert into budget (utilized_budget)
+         select sum(salary) from role`
+    )
+    const [sum, fields] = await connection.promise().query(
+        `select * from budget`
+    )
+    console.table(sum)
+}
+
+module.exports = {
+    viewEmployeesQ, viewRolesQ, viewDepartmentsQ, addEmployeeQ,
+    updateRoleQ, addDepartmentQ, addRoleQ, seeBudgetQ
+}
